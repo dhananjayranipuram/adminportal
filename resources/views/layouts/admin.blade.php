a4356b61c46a0bd202a7b333d8928f985eb7328e
dhananjaydevmonkey <dhananjaya.k@devmonkey.in>
2025-05-19 20:25:55 +0530

    title added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <title>Admin Portal</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
